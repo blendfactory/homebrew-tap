0b13330dfc0ccd46c39df61577688bf85e7897af
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-17 08:14:26 +0900

    feat: Create Dvm formula template

---
+++ dvm_template.rb
@@ -0,0 +1,28 @@
+class Dvm < Formula
+  version "{{ version }}"
+  desc "Dart Version Management: A simple CLI to manage Dart SDK versions per project."
+  homepage "https://github.com/blendfactory/dvm"
+  
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/blendfactory/dvm/releases/download/#{version}/dvm-#{version}-macos-arm64.tar.gz"
+      sha256 "{{ sha256_macos_arm64 }}"
+    else
+      url "https://github.com/blendfactory/dvm/releases/download/#{version}/dvm-#{version}-macos-x64.tar.gz"
+      sha256 "{{ sha256_macos_x64 }}"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/blendfactory/dvm/releases/download/#{version}/dvm-#{version}-linux-x64.tar.gz"
+    sha256 "{{ sha256_linux_x64 }}"
+  end
+
+  def install
+    bin.install "dvm"
+  end
+
+  test do
+    assert_equal "#{version}", shell_output("#{bin}/dvm --version").strip
+  end
+end
