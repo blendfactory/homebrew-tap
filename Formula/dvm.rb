0bacce3f6e0c2b10bc3c54286a1dd2e856be627a
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-08-12 18:14:20 +0900

    fix: Fix download url

--- Formula/dvm.rb
+++ Formula/dvm.rb
@@ -8,14 +8,14 @@ class Dvm < Formula
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/blendfactory/dvm/releases/download/#{version}/dvm-#{version}-macos-arm64.tar.gz"
+      url "https://github.com/blendfactory/dvm/releases/download/#{version}/dvm-0.0.8.1-macos-arm64.tar.gz"
       sha256 "2bceae1f887e08d3b821ec5198adcfd3bfbe4c56eb26061989a929b2e01bd7e2"
     else
-      url "https://github.com/blendfactory/dvm/releases/download/#{version}/dvm-#{version}-macos-x64.tar.gz"
+      url "https://github.com/blendfactory/dvm/releases/download/#{version}/dvm-0.0.8.1-macos-x64.tar.gz"
       sha256 "50603ff19d8be02ca3446b682b250e5399b5c0b2911d0fa671e5a0a07f14a323"
     end
   elsif OS.linux?
-    url "https://github.com/blendfactory/dvm/releases/download/#{version}/dvm-#{version}-linux-x64.tar.gz"
+    url "https://github.com/blendfactory/dvm/releases/download/#{version}/dvm-0.0.8.1-linux-x64.tar.gz"
     sha256 "87faff98a7362ab61e41491aff040dfe11f42030317c80e590667b0a409fbc9c"
   end
 
